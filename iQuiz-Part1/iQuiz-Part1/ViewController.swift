02e3c38077a070d0625860682ed0127d268b7e6a
Timothy <timothy.chongz.luong@gmail.com>
2016-05-15 22:34:01 -0700

    iQuiz Part 1

---
+++ iQuiz-Part1/iQuiz-Part1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iQuiz-Part1
+//
+//  Created by Timothy Luong on 5/15/16.
+//  Copyright © 2016 Timothy Luong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
